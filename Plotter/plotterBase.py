e48340254d2750215e7e64fd12ddcf0271f23450
Jin Choi <jin.choi@cern.ch>
2020-10-19 16:28:38 +0900

    plotter update

--- Plotter/plotterBase.py
+++ Plotter/plotterBase.py
@@ -17,24 +17,46 @@ class plotterBase:
         else:
             raise(NameError)
         
-    def get_hists(self, hists_data={}, hists_exp={}):
+    def get_hists(self, hists_data={}, hists_exp={}, rebin=-1):
         self.hists_data = {}
         self.hists_exp = {}
         self.hists_data = hists_data
         self.hists_exp = hists_exp
 
-    def decorate(self, deco_type="none", x_title="", y_title=""):
+        if rebin == -1:
+            pass
+        else:
+            for name, hist in self.hists_data.items():
+                hist.Rebin(rebin)
+            for name, hist in self.hists_exp.items():
+                hist.Rebin(rebin)
+
+    def decorate(self, deco_type="none", x_title="", y_title="", x_max=-1.):
         # none, normalize, ratio
         self.decos_data = {}
         self.decos_exp = {}
+        
         if deco_type == "none":
             # TODO: make function for default deco_type
             color = 2
+
+            y_range = -1.
+            for name, hist in self.hists_exp.items():
+                this_y_range = hist.GetMaximum()
+                if y_range < this_y_range:
+                    y_range = this_y_range
+            
             for name, hist in self.hists_exp.items():
                 h = hist.Clone("clone_" + name)
                 h.SetLineColor(color)
                 color += 1
-                y_range = h.GetMaximum()
+
+                if x_max == -1.:
+                    pass
+                else:
+                    h.GetXaxis().SetRangeUser(0, x_max)
+                
+                #y_range = h.GetMaximum()
                 h.GetYaxis().SetRangeUser(0, y_range*1.3)
                 if self.logy:
                     h.GetYaxis().SetRangeUser(1, y_range*10)
@@ -47,20 +69,36 @@ class plotterBase:
         elif deco_type == "normalize":
             # TODO: hists_data decoration setup
             color = 2
+
+            # get y_range
+            y_range = -1.
+            for name, hist in self.hists_exp.items():
+                scale_factor = 1. / hist.Integral()
+                hist.Scale(scale_factor)
+
+                this_y_range = hist.GetMaximum()
+                if y_range < this_y_range:
+                    y_range = this_y_range
+
             for name, hist in self.hists_exp.items():
                 #print(name, type(hist))
                 h = hist.Clone("clone_" + name)
                 
                 # scale histogram
-                scale_factor = 1. / h.Integral()
-                h.Scale(scale_factor)
+                #scale_factor = 1. / h.Integral()
+                #h.Scale(scale_factor)
 
                 # set color
                 h.SetLineColor(color)
                 color += 1
-
+                
+                # x range
+                if x_max == -1.:
+                    pass
+                else:
+                    h.GetXaxis().SetRangeUser(0, x_max)
                 # y range
-                y_range = h.GetMaximum()
+                #y_range = h.GetMaximum()
                 h.GetYaxis().SetRangeUser(0, y_range*1.3)
                 if self.logy:
                     h.GetYaxis().SetRangeUser(1, y_range*10)
@@ -82,8 +120,8 @@ class plotterBase:
         else:
             raise(NameError)
 
-    def canvas(self, deco_type="normalize", x_title="", y_title=""):
-        self.decorate(deco_type, x_title, y_title)
+    def canvas(self, deco_type="normalize", x_title="", y_title="", x_max=-1.):
+        self.decorate(deco_type, x_title, y_title, x_max)
         self.cvs.SetGrid(1)
         self.cvs.cd()
         if self.cvs_type == "normal":
